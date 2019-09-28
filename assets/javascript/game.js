520b8e8c3be5faa3bc0a80e101bf80ff533a71c7
Daniel Groveman <daniel5075@gmail.com>
2019-09-28 08:08:47 -0500

    adding images, update to html to display buttons

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//create crystals displayed as buttons
+//generate random numbers for game
+//generate random numbers for each crystal
+//on click add crystal number to total number
+//check after each click if total number equals games random number, if less keep playing, if equal-win
